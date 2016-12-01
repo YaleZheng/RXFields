14ff9df783916fb9ed93f35c5f5636cfabdbc49a
yale.zheng <yale.zheng@icloud.com>
2016-12-01 09:52:31 +0800

    init project content

--- app/src/main/java/io/yale/rxfields/MainActivity.java
+++ app/src/main/java/io/yale/rxfields/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import io.yale.rxfields.ilb.RxInt;
+
 import static io.yale.tinyuikit.lib.ViewExtension.v_findView;
 import static io.yale.tinyuikit.lib.ViewExtension.v_setClickListener;
 import static io.yale.tinyuikit.lib.ViewExtension.v_setText;
@@ -22,8 +24,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.act_main);
 
         final TextView countLabel = v_findView(this, R.id.label_count);
+        v_setClickListener(this, R.id.btn_increase, v -> vm.count.add(1));
+        v_setClickListener(this, R.id.btn_decrease, v -> vm.count.add(-1));
+
+        vm.count.ob()
+                .subscribe(count -> v_setText(countLabel, count.toString()));
     }
 
     private static class PageVM {
+        RxInt count = new RxInt(0);
     }
 }
